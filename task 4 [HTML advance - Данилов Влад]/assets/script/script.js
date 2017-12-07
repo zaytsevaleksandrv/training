bd2cb46acffb8ff469c17ad1104e3a253da8b4c1
Зайцев Александр Вячеславович <zaytsev.aleksandr.v@gmail.com>
2017-12-07 11:38:03 +0400

    script

---
+++ task 4 [HTML advance - Данилов Влад]/assets/script/script.js
@@ -0,0 +1,11 @@
+function onPlayFunction() {
+  alert('Вы меня включили!')
+}
+
+function onPlayAutoFunction() {
+  alert('Меня заставили включиться!')
+}
+
+function onPauseFunction() {
+  alert('Вы меня отключили!')
+}
