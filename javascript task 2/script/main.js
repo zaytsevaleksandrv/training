d5ac5f0dc92035e8b4f75398ec813f68b54e60d4
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-02-02 14:10:24 +0400

    setInterval

--- javascript task 2/script/main.js
+++ javascript task 2/script/main.js
@@ -19,11 +19,56 @@ var c = sumWithCall.call(o, number);
 console.log('call: ' + c);
 
 //5. setInterval
-let output = setInterval(function outputText() {
-  console.log('setInterval: text');
-}, 2000);
+var num = 0;
+var max = 5;
+var interval = null;
 
-setTimeout(function() {
-  clearInterval(output);
-  console.log('setInterval: stop');
-}, 10000)
+function incrementNumber() {
+  num++;
+  console.log('setInterval: text ' + num);
+
+  if (num == max) {
+    clearInterval(interval);
+    console.log('setInterval: stop, выполненно - ' + num + ' раз');
+  }
+}
+
+interval = setInterval(incrementNumber, 2000);
+// let output = setInterval(function outputText() {
+//   console.log('setInterval: text');
+// }, 2000);
+//
+// setTimeout(function() {
+//   clearInterval(output);
+//   console.log('setInterval: stop');
+// }, 10000)
+
+
+// function timeStamp(number) {
+//   return this.currentValue += number;
+// }
+// var number = 1000;
+// var o = {currentValue: 0};
+// var g = sumWith.bind(o);
+
+
+
+
+// setInterval(function outputTwo() {
+//   console.log('setInterval: timeStamp');
+// }, time);
+//
+// setTimeout(function() {
+//   clearInterval(outputTwo);
+//   console.log('setInterval: timeStamp stop');
+// }, 25000)
+
+// let outputTwo = setInterval(function outputText() {
+//   console.log('setInterval: timeStamp');
+//   console.log(i);
+// }, i++);
+//
+// setTimeout(function() {
+//   clearInterval(outputTwo);
+//   console.log('setInterval: timeStamp stop');
+// }, 10000)
