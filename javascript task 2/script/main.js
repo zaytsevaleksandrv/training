22655b74715483c592ddb23e7c0e34710dd56494
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-02-02 12:35:38 +0400

    Контекст вызова и карринг

--- javascript task 2/script/main.js
+++ javascript task 2/script/main.js
@@ -1,5 +1,5 @@
 //Контекст вызова и карринг
-
+//1 сспособ
 function sumWith(number) {
   return this.currentValue += number;
 }
@@ -7,4 +7,13 @@ var number = 2;
 var o = {currentValue: 3};
 var g = sumWith.bind(o);
 
-alert(g(number));
+alert('bind: ' + g(number));
+
+//2 способ
+function sumWithCall(number) {
+  return this.currentValue += number;
+}
+var number = 2;
+var o = {currentValue: 3};
+var c = sumWithCall.call(o, number);
+alert('call: ' + c);
