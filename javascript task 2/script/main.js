f71a9f55f00975f549b3eef6de16d6e3532e4509
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-02-02 13:03:40 +0400

    setInterval

--- javascript task 2/script/main.js
+++ javascript task 2/script/main.js
@@ -1,4 +1,4 @@
-//Контекст вызова и карринг
+//4. Контекст вызова и карринг
 //1 сспособ
 function sumWith(number) {
   return this.currentValue += number;
@@ -7,7 +7,7 @@ var number = 2;
 var o = {currentValue: 3};
 var g = sumWith.bind(o);
 
-alert('bind: ' + g(number));
+console.log('bind: ' + g(number));
 
 //2 способ
 function sumWithCall(number) {
@@ -16,4 +16,14 @@ function sumWithCall(number) {
 var number = 2;
 var o = {currentValue: 3};
 var c = sumWithCall.call(o, number);
-alert('call: ' + c);
+console.log('call: ' + c);
+
+//5. setInterval
+let output = setInterval(function outputText() {
+  console.log('setInterval: text');
+}, 2000);
+
+setTimeout(function() {
+  clearInterval(output);
+  console.log('setInterval: stop');
+}, 10000)
