dffcbdc7056dcbddbf3208718adab2f5e2af9752
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-02-12 00:05:51 +0400

    4 task

--- 11. JS Advanced. Prototype and OOP/script/main.js
+++ 11. JS Advanced. Prototype and OOP/script/main.js
@@ -401,3 +401,50 @@ console.log('%cmartyTruck','color:#f8ab47;font-weight: bold', 'open doors: ' + m
 
 console.log('%c\n' + martyTruck.toString(), 'color: orange')
 console.log('%c\n' + martyTruck.valueOf(), 'color: green')
+
+
+// 4 a
+
+function Animal(name) {
+  this.name = name;
+}
+
+Animal.prototype.printName = function() {
+  console.log(this.name);
+}
+
+
+function Rabbit() {
+  Animal.apply(this, arguments);
+}
+
+Rabbit.prototype.printName = function() {
+  console.log(this.name);
+}
+
+Rabbit.prototype = Object.create(Rabbit.prototype);
+Rabbit.prototype.constructor = Rabbit;
+
+var r = new Rabbit('Lucky');
+r.printName(); // Should run without any errors and write 'Lucky'
+console.log(Rabbit.prototype.hasOwnProperty('printName')) // Should be false
+
+
+// 4 b
+var animal = {
+  speed: 0
+};
+
+var mammal = {
+  age: 3
+};
+
+var rabbit = {
+  name: 'Lucky'
+};
+
+animal.__proto__ = mammal;
+rabbit.__proto__ = animal;
+
+console.log(rabbit.age) // Should be '3'
+console.log(rabbit.hasOwnProperty("age")) // Should be 'false' (also fix error)
