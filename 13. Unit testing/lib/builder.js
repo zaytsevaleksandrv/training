08cb9b1915bd784b839f274a75d913e8d42c4966
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-02-19 23:46:37 +0400

    unit test edited

--- 13. Unit testing/lib/builder.js
+++ 13. Unit testing/lib/builder.js
@@ -1,46 +1,58 @@
-function CarBuilder() {}
+function Car(seat, tripComputed, gps, size) {
+    this.seat = seat;
+    this.tripComputed = tripComputed;
+    this.gps = gps;
+    this.size = size;
+}
+Car.prototype.getSeats = function() {
+    return this.seat;
+}
+Car.prototype.getTripComputer = function () {
+    return this.tripComputed;
+}
+Car.prototype.getGps = function () {
+    return this.gps;
+}
+Car.prototype.getTyreSize = function () {
+    return this.size;
+}
+
+function CarBuilder() {
+    this.child = new Car();
+    this.child2 = new Car();
+    this.reset();
+}
 CarBuilder.prototype.reset = function() {
-    this.seat = 0;
-    this.tripComputed = 0;
-    this.gps = 0;
-    this.size = 0;
+    this.child.seat = 4;
+    this.child.tripComputed = false;
+    this.child.gps = false;
+    this.child.size = 'medium';
     return this;
-};
+}
 CarBuilder.prototype.setSeats = function(seat) {
-  this.seat = seat || 2;
-  return this;
-};
+    this.child.seat = seat;
+    return this;
+}
 CarBuilder.prototype.setTripComputer = function(tripComputed) {
-  this.tripComputed = tripComputed || true;
-  return this;
-};
+    this.child.tripComputed = tripComputed;
+    return this;
+}
 CarBuilder.prototype.setGPS = function(gps) {
-  this.gps = gps || true;
-  return this;
-};
+    this.child.gps = gps;
+    return this;
+}
 CarBuilder.prototype.setTyreSize = function(size) {
-  this.size = size || 'big';
-  return this;
-};
+    this.child.size = size;
+    return this;
+}
 CarBuilder.prototype.getResult = function() {
-    return { 
-        getSeats: function() {
-            CarBuilder.prototype.setSeats.call(this);
-            return this.seat;
-        }, 
-        getTripComputer: function() {
-            CarBuilder.prototype.setTripComputer.call(this);
-            return this.tripComputed;
-        }, 
-        getGps: function() {
-            CarBuilder.prototype.setGPS.call(this);
-            return this.gps;
-        },
-        getTyreSize: function() {
-            CarBuilder.prototype.setTyreSize.call(this);
-            return this.size;
-        } 
-    };
+    var Lada = new Car(
+        this.child.seat,
+        this.child.tripComputed,
+        this.child.gps,
+        this.child.size
+    );
+    return Lada;
 }
 
 module.exports = CarBuilder;
